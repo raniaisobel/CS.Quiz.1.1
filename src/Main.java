44babb3625bd3618059768d92387d6fe87ec49f5
Rania <rania.isobel@gmail.com>
2024-08-28 09:21:26 +0800

    Finished formative!

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Rania Sutton
 *****************************************************************************************************
 */
-
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        int age = 0;
+        String firstName = "";
+        String favouriteFood = "";
+        System.out.println("Please enter your age: ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Please enter your first name: ");
+        firstName = scanner.nextLine();
+        System.out.println("Please enter your favourite food: ");
+        favouriteFood = scanner.nextLine();
+        System.out.println("First name: "+firstName+"\nAge: "+age+"\nFavourite food: "+favouriteFood);
     }
 
 }
